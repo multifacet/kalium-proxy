da394743f8a82cd4ce2dfaa971b26a2c9ad7a0de
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2023-02-10 15:57:35 -0600

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt update
+
+echo "Building lz4"
+git clone https://github.com/lz4/lz4
+pushd lz4
+sudo make install
+popd
+
+echo "Building libzmq"
+sudo apt remove libunwind-dev
+wget https://github.com/zeromq/libzmq/releases/download/v4.2.2/zeromq-4.2.2.tar.gz
+tar -xvf zeromq-4.2.2.tar.gz
+pushd
+cd zeromq-4.2.2
+mkdir -p build
+cd build
+../configure
+sudo make install -j$(nproc)
+sudo ldconfig
+popd
+
+echo "Building czmq"
+wget https://github.com/zeromq/czmq/releases/download/v4.2.0/czmq-4.2.0.tar.gz
+tar -xvf czmq-4.2.0.tar.gz
+pushd czmq-4.2.0
+mkdir -p build
+cd build
+../configure
+sudo make install
+sudo ldconfig
+popd
+
+git checkout artifact
+go build -o seclambda
