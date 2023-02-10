5be1aff1c88a9073f595e2755708127e45a57730
root <root@node0.netes2.cs799-serverless-pg0.wisc.cloudlab.us>
2023-02-10 16:38:48 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -12,10 +12,13 @@ popd
 
 echo "Building libzmq"
 sudo apt remove libunwind-dev
+sudo apt install -y \
+    git build-essential libtool \
+    pkg-config autotools-dev autoconf automake cmake \
+    uuid-dev libpcre3-dev valgrind liblzma-dev
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.2/zeromq-4.2.2.tar.gz
 tar -xvf zeromq-4.2.2.tar.gz
-pushd
-cd zeromq-4.2.2
+pushd zeromq-4.2.2
 mkdir -p build
 cd build
 ../configure
@@ -29,10 +32,13 @@ tar -xvf czmq-4.2.0.tar.gz
 pushd czmq-4.2.0
 mkdir -p build
 cd build
-../configure
+cmake ..
 sudo make install
 sudo ldconfig
 popd
 
 git checkout artifact
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 go build -o seclambda
